1bbfbdec644f6c4eed202d4fbb7a9614dc2848ca
Aichi <yassine.aichi11@gmail.com>
2025-02-13 19:35:16 +0100

    new stuff

--- public/js/js.js
+++ public/js/js.js
@@ -4,3 +4,16 @@ import { inject } from "@vercel/analytics"
 injectSpeedInsights();
 inject();
 
+
+document.getElementById("about").onclick = function () {
+    window.location.href = "about.html";
+}
+
+document.getElementById("contact").onclick = function () {
+    window.location.href = "contact.html";
+}
+
+
+document.getElementById("projects").onclick = function () {
+    window.location.href = "projects.html";
+}
