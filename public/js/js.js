6a3c1e21f527163f776c82c9465814b3557486b2
Aichi <yassine.aichi11@gmail.com>
2025-02-13 19:59:18 +0100

    working pages

--- public/js/js.js
+++ public/js/js.js
@@ -5,15 +5,3 @@ injectSpeedInsights();
 inject();
 
 
-document.getElementById("about").onclick = function () {
-    window.location.href = "about.html";
-}
-
-document.getElementById("contact").onclick = function () {
-    window.location.href = "contact.html";
-}
-
-
-document.getElementById("projects").onclick = function () {
-    window.location.href = "projects.html";
-}
---
+++ public/js/new.js
@@ -0,0 +1,26 @@
+
+document.getElementById("about").onclick = function (event) {
+    event.preventDefault();
+    window.location.href = "about.html";
+}
+
+document.getElementById("home").onclick = function (event) {
+    event.preventDefault();
+    window.location.href = "index.html";
+}
+
+document.getElementById("norselle").onclick = function (event) {
+    event.preventDefault();
+    window.location.href = "index.html";
+}
+
+document.getElementById("contact").onclick = function (event) {
+    event.preventDefault();
+    window.location.href = "contact.html";
+}
+
+
+document.getElementById("projects").onclick = function (event) {
+    event.preventDefault();
+    window.location.href = "projects.html";
+}
